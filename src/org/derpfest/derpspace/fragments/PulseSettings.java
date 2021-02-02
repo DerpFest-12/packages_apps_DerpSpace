ba3be975904896e7e4e23f2658c0f6afc7e7c322
TikkiTikki <radstylemusic@gmail.com>
2021-02-02 15:01:50 -0300

    DerpSpace: Pulse: Custom gravity and Center mirrored [2/2]
    Change-Id: I13901c934f6c593132061755bf77a0203d0abb5c
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/org/derpfest/derpspace/fragments/PulseSettings.java
+++ src/org/derpfest/derpspace/fragments/PulseSettings.java
@@ -53,6 +53,8 @@ public class PulseSettings extends SettingsPreferenceFragment implements
     private static final String PULSE_RENDER_CATEGORY_SOLID = "pulse_2";
     private static final String PULSE_RENDER_CATEGORY_FADING = "pulse_fading_bars_category";
     private static final String PULSE_RENDER_MODE_KEY = "pulse_render_style";
+    private static final String PULSE_CUSTOM_GRAVITY = "pulse_custom_gravity";
+    private static final String VISUALIZER_CENTER_MIRRORED = "visualizer_center_mirrored";
     private static final int RENDER_STYLE_FADING_BARS = 0;
     private static final int RENDER_STYLE_SOLID_LINES = 1;
     private static final int COLOR_TYPE_ACCENT = 0;
@@ -67,9 +69,11 @@ public class PulseSettings extends SettingsPreferenceFragment implements
     private SwitchPreference mQsPulse;
     private SwitchPreference mAmbientPulse;
     private SwitchPreference mPulseSmoothing;
+    private SwitchPreference mPulseCenterMirrored;
     private Preference mRenderMode;
     private ListPreference mColorModePref;
     private ColorPickerPreference mColorPickerPref;
+    private ListPreference mPulseGravity;
     private Preference mLavaSpeedPref;
     private Preference mFooterPref;
 
@@ -122,6 +126,10 @@ public class PulseSettings extends SettingsPreferenceFragment implements
                 PULSE_RENDER_CATEGORY_SOLID);
 
         mPulseSmoothing = (SwitchPreference) findPreference(PULSE_SMOOTHING_KEY);
+
+        mPulseCenterMirrored = (SwitchPreference) findPreference(VISUALIZER_CENTER_MIRRORED);
+
+        mPulseGravity = (ListPreference) findPreference(PULSE_CUSTOM_GRAVITY);
 
         mFooterPref = findPreference(PULSE_SETTINGS_FOOTER);
         mFooterPref.setTitle(R.string.pulse_help_policy_notice_summary);
@@ -182,6 +190,9 @@ public class PulseSettings extends SettingsPreferenceFragment implements
 
         mPulseSmoothing.setEnabled(navbarPulse || lockscreenPulse || qsPulse || ambientPulse);
 
+        mPulseCenterMirrored.setEnabled(navbarPulse || lockscreenPulse || qsPulse || ambientPulse);
+
+        mPulseGravity.setEnabled(navbarPulse || lockscreenPulse || qsPulse || ambientPulse);
 
         mColorModePref.setEnabled(navbarPulse || lockscreenPulse || qsPulse || ambientPulse);
         if (navbarPulse || lockscreenPulse || qsPulse) {
